2e327e35f55ff0dbe9b1c2336eff5e7a370110d7
Manish <manish160993@gmail.com>
2019-05-31 19:43:59 -0500

    Application update

---
+++ ML/readme.txt
@@ -0,0 +1,6 @@
+- Install dependencies using "pip Install requirements.txt"
+- To run data pre-processing:  python3 data_pre_processing.py ../shuffled-full-set-hashed.csv
+    Usage: - python3 data_pre_processing.py <full-path-to-data>
+- To run training of model: python3 train_model.py
+- Look at Jupyter Notebook to see experiment done in deep learning
+- To classify new documents, run: python3 document_classifier.py <test-string>
--- UI/app.py
+++ UI/app.py
@@ -6,7 +6,7 @@ from flask import Flask, request
 from static.document_classifier import classify_input
 
 # Create an instance of Flask and declare variables for our model
-app = Flask(__name__)
+application = Flask(__name__)
 clf = pickle.load(open('./static/saved_data_objects/LRModel.pkl', 'rb'))
 vectorizer = pickle.load(open('./static/saved_data_objects/vectorizer.pkl','rb'))
 id_to_category = pickle.load(open('./static/saved_data_objects/id_to_category.pkl','rb'))
@@ -14,12 +14,12 @@ id_to_category = pickle.load(open('./static/saved_data_objects/id_to_category.pk
 
 
 # Homepage, render input prompt HTML
-@app.route('/')
+@application.route('/')
 def index():
     return flask.render_template('index.html')
     
 # Result page, get prediction result and render
-@app.route('/predict', methods=['GET', 'POST'])
+@application.route('/predict', methods=['GET', 'POST'])
 # Handle request
 def form():
     # Document content was sent with POST request
@@ -29,7 +29,5 @@ def form():
         result = classify_input(data, clf, vectorizer, id_to_category)
         return flask.render_template('result.html', document = data, result=result)
 
-# Get prediction
-def getresult(model, vec, data):
-    transformed_data = vec.transform([data])
-    return model.predict(transformed_data)
+if __name__ == '__main__':
+    application.run()
